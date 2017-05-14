d98c484b9dd94d2362a13ab65959fe994fa38aaf
GhostName <painandwound@outlook.com>
2017-05-14 23:40:43 +0700

    Update Board

--- src/scrabble.c
+++ src/scrabble.c
@@ -8,6 +8,60 @@
 
 #define CARDS 100
 
+typedef struct
+{
+char tile[3] ;
+int letterX ;
+int wordX ;
+
+}_board ;
+
+_board field [16][16] ; //Use only 15*15
+
+void createBoard ()
+{
+
+    int i = 0;
+    int j = 0;
+
+    for (i = 0 ; i < 15 ;i++)
+        {
+
+        for (j = 0 ;j <15 ; j++)
+            {
+            field[i][j].tile[0] = ' ' ;
+            field[i][j].letterX = 1 ;
+            field[i][j].wordX = 1;
+
+            if ( (i == 0 || i == 7 || i == 14 ) && ( j== 0 || j==7 || j== 14  )  )
+                field[i][j].wordX = 3 ;
+
+        else if ( (i == 5 || i == 9) && (j==1 || j==5 || j==9 || j==13) )
+                field[i][j].letterX = 3 ;
+        else if ( (i==1 || i==13 ) && ( j==5 || j==9 ) )
+                field[i][j].letterX = 3 ;
+        else if ( (i == 3 || i== 11) && ( j== 0 ||j==7 || j == 14) )
+                field[i][j].letterX = 2 ;
+        else if ( (i == 0 || i== 7  || i==14 ) && (j==3 || j == 11) )
+                field[i][j].letterX = 2 ;
+        else if ((i==2|| i==6 || i==8 || i==12) && (j==2 || j== 6 || j==8 || j==12) )
+                field[i][j].letterX = 2 ;
+        else if ((i== 1 || i== 13 ) && (j== 1|| j== 13 ))
+                field[i][j].wordX = 2;
+        else if ((i == 2 || i== 12 ) && (j== 2|| j==12 ))
+                field[i][j].wordX = 2;
+        else if ((i == 3 || i== 11 ) && (j==3 || j==11 ))
+                field[i][j].wordX = 2;
+        else if ((i == 4 || i== 10 ) && (j==4 || j==10 ))
+                field[i][j].wordX = 2;
+            }
+
+
+        }
+
+}
+
+
 int main()
 {
   int deck[CARDS];
@@ -20,6 +74,8 @@ int main()
   printf(">Play\n");
   printf(">Deck\n");
   printf(">Resume\n");
+
+  createBoard();
 
   DECK_T Tile[100];
 
